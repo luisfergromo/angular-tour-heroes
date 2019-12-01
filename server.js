3f5ceee130960c8fa185de73c2f510508b6f6a0a
Luis <is696372@iteso.mx>
2019-12-01 15:03:12 -0600

    Realease

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-tour-of-herores'));
+app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
 
 app.get('/*', function (req, res) {
 
